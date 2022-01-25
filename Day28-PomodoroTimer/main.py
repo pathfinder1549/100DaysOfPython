1287eda863999ff8c096d90b166099760934e11b
pathfinder1549 <mhilliard117@gmail.com>
2022-01-25 17:06:35 -0500

    timer mechanics

--- Day28-PomodoroTimer/main.py
+++ Day28-PomodoroTimer/main.py
@@ -1,5 +1,8 @@
 from tkinter import *
 import os
+import time
+import math
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -11,36 +14,84 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
+reps = 0
+marks_txt = ""
+
 # ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    pass
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    global reps
+    reps += 1
+
+    work_sec = 10 #WORK_MIN * 60
+    short_break_sec = 5 #SHORT_BREAK_MIN * 60
+    long_break_sec = 20 #LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        top_label.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        top_label.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        top_label.config(text="Work", fg=GREEN)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    global reps
+    global marks_txt
+
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    #if count_sec == 0:
+    #    count_sec = "00"
+    #elif count_sec < 10:
+    #    count_sec = f"0{count_sec}"
+    count_min = f"{count_min:02}"
+    count_sec = f"{count_sec:02}"
+
+    tomato.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count >= 0:
+        window.after(1000, count_down, count-1)
+    else:
+        start_timer()
+        if reps % 2 == 0:
+            marks_txt += CHECK
+            check_marks.config(text=marks_txt)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+
+
 base_folder = os.path.dirname(__file__)
 image_path = os.path.join(base_folder, "tomato.png")
 
-top_label = Label(text="Timer", font=(FONT_NAME, 25, "bold"), fg= GREEN, bg=YELLOW)
+top_label = Label(text="Timer", font=(FONT_NAME, 40, "bold"), fg= GREEN, bg=YELLOW)
 top_label.grid(column=1, row=0)
 
 tomato = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file=image_path)
 tomato.create_image(100, 112, image=tomato_img)
-tomato.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = tomato.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 36, "bold"))
 tomato.grid(column=1, row=1)
 
-start_button = Button(text="Start", bg=PINK)
+start_button = Button(text="Start", command=start_timer, bg=PINK)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset", bg=PINK)
+reset_button = Button(text="Reset", command=reset_timer, bg=PINK)
 reset_button.grid(column=2, row=2)
 
-check_marks = Label(text=CHECK, font=(FONT_NAME, 16, "normal"), fg=GREEN, bg=YELLOW)
+check_marks = Label(font=(FONT_NAME, 16, "normal"), fg=GREEN, bg=YELLOW)
 check_marks.grid(column=1, row=3)
+
+#count_down(5)
 
 window.mainloop()
