10dba3b20ae0931c61d482b0cfdb34bec6836dfc
pathfinder1549 <mhilliard117@gmail.com>
2022-01-26 14:10:32 -0500

    added ui elements

--- Day29-PasswordManager/main.py
+++ Day29-PasswordManager/main.py
@@ -1,10 +1,15 @@
 from tkinter import *
 import os
 
+#FONT = ("Ariel", 12, "normal")
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def gen_password():
+    pass
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def add_entry():
+    pass
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -17,6 +22,30 @@ image_path = os.path.join(base_folder, "logo.png")
 canvas = Canvas(width=200, height=200)
 logo_img = PhotoImage(file=image_path)
 canvas.create_image(100, 100, image=logo_img)
-canvas.grid(column=1, row=1)
+canvas.grid(column=1, row=0)
+
+site_label = Label(text="Website:")
+site_label.grid(column=0, row=1)
+
+user_label = Label(text="Email/Username:")
+user_label.grid(column=0, row=2)
+
+pass_label = Label(text="Password:")
+pass_label.grid(column=0, row=3)
+
+site_entry = Entry(width=35)
+site_entry.grid(column=1, row=1, columnspan=2)
+
+user_entry = Entry(width=35)
+user_entry.grid(column=1, row=2, columnspan=2)
+
+pass_entry = Entry(width=21)
+pass_entry.grid(column=1, row=3)
+
+gen_button = Button(text="Generate Password", command=gen_password)
+gen_button.grid(column=2, row=3)
+
+add_button = Button(text="Add", command=add_entry, width=36)
+add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
