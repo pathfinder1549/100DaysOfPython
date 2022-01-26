3ae9d7fa743f0c2ad94008165a572f588b4a81c9
pathfinder1549 <mhilliard117@gmail.com>
2022-01-26 13:46:51 -0500

    initial ui setup

--- Day29-PasswordManager/main.py
+++ Day29-PasswordManager/main.py
@@ -12,4 +12,11 @@ window.title("Password Manager")
 window.config(padx=20, pady=20)
 
 base_folder = os.path.dirname(__file__)
-image_path = os.path.join(base_folder, "tomato.png")
+image_path = os.path.join(base_folder, "logo.png")
+
+canvas = Canvas(width=200, height=200)
+logo_img = PhotoImage(file=image_path)
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(column=1, row=1)
+
+window.mainloop()
