59a5902db585df308082779c6dad6d5a8fdf4aa8
pathfinder1549 <mhilliard117@gmail.com>
2022-01-28 17:41:42 -0500

    cleaned up password generator

--- Day29-PasswordManager/main.py
+++ Day29-PasswordManager/main.py
@@ -1,35 +1,26 @@
-from email import message
 from tkinter import *
 from tkinter import messagebox
+from random import randint, choice, shuffle
 import os
-import random
-
-#FONT = ("Ariel", 12, "normal")
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def gen_password():
-    #Password Generator Project
+    """Generate randomized password"""
+
+    # refactored from day 5 project
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-    nr_letters = random.randint(8, 10)
-    nr_symbols = random.randint(2, 4)
-    nr_numbers = random.randint(2, 4)
-
-    letter_list = [random.choice(letters) for n in range(nr_letters)]
-    symbol_list = [random.choice(symbols) for n in range(nr_symbols)]
-    number_list = [random.choice(numbers) for n in range(nr_numbers)]
+    letter_list = [choice(letters) for n in range(randint(8, 10))]
+    symbol_list = [choice(symbols) for n in range(randint(2, 4))]
+    number_list = [choice(numbers) for n in range(randint(2, 4))]
     password_list = letter_list + symbol_list + number_list
-    random.shuffle(password_list)
-
-    password = ""
-    for char in password_list:
-        password += char
+    shuffle(password_list)
+    password = "".join(password_list)
 
     pass_entry.delete(0, END)
     pass_entry.insert(0, password)
-    #print(f"Your password is: {password}")
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def add_entry():
