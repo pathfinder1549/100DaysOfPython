56556322584b3d0c731821c55011695384c29d18
pathfinder1549 <mhilliard117@gmail.com>
2022-01-27 20:51:07 -0500

    finish alphabet exercise

--- Day30-ExceptionsJSON/alphabet.py
+++ Day30-ExceptionsJSON/alphabet.py
@@ -10,22 +10,17 @@ file_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 alpha_dict = {row.letter:row.code for (index,row) in file_df.iterrows()}
 
 # create list of phonetics from a user input word
-#def generate_phonetic():
-#    word = input("Enter a word to translate into phonetics: ")
-#    try:
-#        letter_list = [alpha_dict[letter.upper()] for letter in word]
-#    except KeyError:
-#        print("Please enter letters only.")
-#        generate_phonetic()
-#    else:
-#        print(letter_list)
+def generate_phonetic():
+    word = input("Enter a word to translate into phonetics: ")
+    try:
+        letter_list = [alpha_dict[letter.upper()] for letter in word]
+    except KeyError:
+        print("Please enter letters only.")
+        generate_phonetic()
+        # recursive call allows repeat input like while loop
+        # note rest of the function still executes
+    else:
+        print(letter_list)
+    print("recursion!")
 
-#generate_phonetic()
-
-word = input("Enter a word to translate into phonetics: ")
-try:
-    letter_list = [alpha_dict[letter.upper()] for letter in word]
-except KeyError:
-    print("Please enter letters only.")
-else:
-    print(letter_list)
+generate_phonetic()
