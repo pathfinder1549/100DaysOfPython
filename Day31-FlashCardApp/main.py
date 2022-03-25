0b75da7bdcb5e3c62a44f085f492c72be78df096
pathfinder1549 <mhilliard117@gmail.com>
2022-03-25 15:22:56 -0400

    added answer side of card, timer

--- Day31-FlashCardApp/main.py
+++ Day31-FlashCardApp/main.py
@@ -1,5 +1,9 @@
+from asyncio.windows_events import NULL
+from email.mime import image
+from glob import glob
 from tkinter import *
 from tkinter import messagebox
+from turtle import color
 from pandas import *
 from random import choice
 import os
@@ -25,11 +29,24 @@ except FileNotFoundError:
 
 
 ### GENERATE WORDS ###
+def new_card():
+    global flip_timer
+    window.after_cancel(flip_timer)
+    new_word()
+
 def new_word():
-    word_dict = choice(word_list)
-    front_card.itemconfig(title_text, text=FRONT_LANG)
-    front_card.itemconfig(word_text, text=word_dict[FRONT_LANG])
-  
+    global flip_timer
+    current_word = choice(word_list)
+    card.itemconfig(card_image, image=card_front_img)
+    card.itemconfig(title_text, text=FRONT_LANG, fill="black")
+    card.itemconfig(word_text, text=current_word[FRONT_LANG], fill="black")
+    flip_timer = window.after(3000, flip_card, current_word)
+
+def flip_card(current_word):
+    card.itemconfig(card_image, image=card_back_img)
+    card.itemconfig(title_text, text=BACK_LANG, fill="white")
+    card.itemconfig(word_text, text=current_word[BACK_LANG], fill="white")
+
 
 ### UI SETUP ###
 window = Tk()
@@ -43,19 +60,22 @@ front_image_path = os.path.join(base_folder, "images/card_front.png")
 back_image_path = os.path.join(base_folder, "images/card_back.png")
 
 # flash card area
-front_card = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
-front_img = PhotoImage(file=front_image_path)
-front_card.create_image(400, 263, image=front_img)
-title_text = front_card.create_text(400, 150, text="title", fill="black", font=("Ariel", 40, "italic"))
-word_text = front_card.create_text(400, 263, text="word", fill="black", font=("Ariel", 60, "bold"))
-front_card.grid(column=0, row=0, columnspan=2)
+card = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+card_front_img = PhotoImage(file=front_image_path)
+card_back_img = PhotoImage(file=back_image_path)
+card_image = card.create_image(400, 263, image=card_front_img)
+title_text = card.create_text(400, 150, text="title", fill="black", font=("Ariel", 40, "italic"))
+word_text = card.create_text(400, 263, text="word", fill="black", font=("Ariel", 60, "bold"))
+card.grid(column=0, row=0, columnspan=2)
 
 # buttons
 wrong_image = PhotoImage(file=wrong_image_path)
-wrong_button = Button(image=wrong_image, command=new_word, highlightthickness=0, pady=50)
+wrong_button = Button(image=wrong_image, command=new_card, highlightthickness=0, pady=50)
 wrong_button.grid(column=0, row=1)
 right_image = PhotoImage(file=right_image_path)
-right_button = Button(image=right_image, command=new_word, highlightthickness=0, pady=50)
+right_button = Button(image=right_image, command=new_card, highlightthickness=0, pady=50)
 right_button.grid(column=1, row=1)
+
+new_word()
 
 window.mainloop()
