a7c8f1c096fb7f1e53fb4aa5fdc8c6c0ded218b5
Max <pathfinder1549@gmail.com>
2022-09-23 14:12:46 -0400

    add content

--- Day32-BirthdayEmailer/main.py
+++ Day32-BirthdayEmailer/main.py
@@ -1,18 +1,39 @@
 import smtplib
 import datetime as dt
+import os
+import random
 
-#my_email = "testEmail@gmail.com"
-#my_password = "testPW"
-#with smtplib.SMTP("smtp.gmail.com") as connection:
-#    connection.starttls()
-#    connection.login(user=my_email,password=my_password)
-#    connection.sendmail(
-#        from_addr=my_email,
-#        to_addrs="recieverAddress@gmail.com",
-#        msg="Subject:Hello\n\nHello World"
-#    )
+# file config
+base_folder = os.path.dirname(__file__)
+data_path = os.path.join(base_folder, "quotes.txt")
 
-now = dt.datetime.now()
-print(now)
+def email_string(to, subject, body):
+    my_email = "testEmail@gmail.com"
+    my_password = "testPW"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email,password=my_password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=to,
+            msg=f"Subject:{subject}\n\n{body}"
+        )
 
-date_of_birth = dt.datetime(year=1989, month=10, day=17)
+def get_quote():
+    try:
+        with open(data_path, mode="r") as quote_file:
+            all_quotes = quote_file.readlines()
+            quote = random.choice(all_quotes)
+    except FileNotFoundError:
+        print("File not found!")
+    return quote
+
+def is_friday():
+    current_day = dt.datetime.now().weekday()
+    if current_day == 4:    # Monday is 0
+        return True
+    else:
+        return False
+
+if is_friday():
+    print(get_quote())
