0b73531b659f9a4ddbf5f7ee0a0b473738051717
Max <pathfinder1549@gmail.com>
2022-09-23 13:45:03 -0400

    datetime test

--- Day32-BirthdayEmailer/main.py
+++ Day32-BirthdayEmailer/main.py
@@ -1,12 +1,18 @@
 import smtplib
+import datetime as dt
 
-my_email = "testEmail@gmail.com"
-my_password = "testPW"
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email,password=my_password)
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs="recieverAddress@gmail.com",
-        msg="Subject:Hello\n\nHello World"
-    )
+#my_email = "testEmail@gmail.com"
+#my_password = "testPW"
+#with smtplib.SMTP("smtp.gmail.com") as connection:
+#    connection.starttls()
+#    connection.login(user=my_email,password=my_password)
+#    connection.sendmail(
+#        from_addr=my_email,
+#        to_addrs="recieverAddress@gmail.com",
+#        msg="Subject:Hello\n\nHello World"
+#    )
+
+now = dt.datetime.now()
+print(now)
+
+date_of_birth = dt.datetime(year=1989, month=10, day=17)
