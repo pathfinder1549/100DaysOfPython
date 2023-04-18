65fd857a333f6a1640486a5c17987fa604de9847
Max Hilliard <max.hilliard@hypertherm.com>
2023-04-18 11:40:53 -0400

    kanye challenge start

--- Day33-ISSOverheadAPI/main.py
+++ Day33-ISSOverheadAPI/main.py
@@ -1,4 +1,18 @@
 import requests
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
-print(response)
+
+# request package includes error handling
+# returns response object from the request
+response.raise_for_status()
+
+data = response.json()
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+timestamp = data["timestamp"]
+
+print(f"ISS Current Location:\n" +\
+      f"Timestamp: {timestamp}\n" +\
+      f"Longitude: {longitude}\n" +\
+      f"Latitude: {latitude}\n")
+
