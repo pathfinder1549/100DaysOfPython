b5095786f42e70cd94ea4eb6c4ae153a38f33b00
Max Hilliard <44248346+pathfinder1549@users.noreply.github.com>
2023-07-28 14:11:57 -0400

    graph creation, pixel creation

--- Day37-HabitTracker/main.py
+++ Day37-HabitTracker/main.py
@@ -1,12 +1,50 @@
 import requests
 import os
+from dotenv import load_dotenv
 
+base_folder = os.path.dirname(__file__)
+env_path = os.path.join(base_folder, ".env.txt")
+load_dotenv(env_path)                       # load .env in root dir, load env vars (all are strings)
+USERNAME = os.getenv("pixela_username")     
+TOKEN = os.getenv("pixela_token")
+
+# create new user api info
 pixela_endpoint = "https://pixe.la/v1/users"
 user_params = {
-    "token": "l12asdvrrk3j4lkj",
-    "username": "max",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
 
-requests.post()
+# create a new user with params above
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+# create graph api info
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+GRAPH_ID = "graph1"
+graph_params = {
+    "id": GRAPH_ID,
+    "name": "Reading",
+    "unit": "pages",
+    "type": "int",
+    "color": "shibafu",
+}
+headers = {
+    "X-USER-TOKEN": TOKEN,
+}
+
+# create new graph for user
+# response = requests.post(url=graph_endpoint, json=graph_params, headers=headers)
+# print(response.text)
+
+# enter new data api info
+data_endpoint = f"{graph_endpoint}/{GRAPH_ID}"
+data_params = {
+    "date": "20230728",
+    "quantity": "3",
+}
+
+response = requests.post(url=data_endpoint, json=data_params, headers=headers)
+print(response.text)
