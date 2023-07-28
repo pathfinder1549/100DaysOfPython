98b871a34a42ec9e1f43e8761c2706a8c9c6caec
Max Hilliard <44248346+pathfinder1549@users.noreply.github.com>
2023-07-28 13:43:08 -0400

    day 37 init commit

---
+++ Day37-HabitTracker/main.py
@@ -0,0 +1,12 @@
+import requests
+import os
+
+pixela_endpoint = "https://pixe.la/v1/users"
+user_params = {
+    "token": "l12asdvrrk3j4lkj",
+    "username": "max",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+requests.post()
